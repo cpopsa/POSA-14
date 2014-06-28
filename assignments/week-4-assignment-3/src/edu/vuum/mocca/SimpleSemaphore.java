c03513b26fe8ec263077192efe7ec751e2688759
cpopsa <c.popsa@gmail.com>
2014-06-28 15:12:44 +0300

    Week 4

--- assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
+++ assignments/week-4-assignment-3/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -1,66 +1,108 @@
 package edu.vuum.mocca;
-import java.util.concurrent.locks.Condition;
+
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
 
 /**
  * @class SimpleSemaphore
- * 
- * @brief This class provides a simple counting semaphore implementation using
- *        Java a ReentrantLock and a ConditionObject (which is accessed via a
- *        Condition). It must implement both "Fair" and "NonFair" semaphore
- *        semantics, just liked Java Semaphores.
+ *
+ * @brief This class provides a simple counting semaphore
+ *        implementation using Java a ReentrantLock and a
+ *        ConditionObject.  It must implement both "Fair" and
+ *        "NonFair" semaphore semantics, just liked Java Semaphores. 
  */
 public class SimpleSemaphore {
     /**
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
-
+	private ReentrantLock lock;
     /**
-     * Define a Condition that waits while the number of permits is 0.
+     * Define a ConditionObject to wait while the number of
+     * permits is 0.
      */
     // TODO - you fill in here
-
+	private Condition condition;
     /**
      * Define a count of the number of available permits.
      */
-    // TODO - you fill in here. Make sure that this data member will
+    // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
-
-    public SimpleSemaphore(int permits, boolean fair) {
-        // TODO - you fill in here to initialize the SimpleSemaphore,
-        // making sure to allow both fair and non-fair Semaphore
-        // semantics.
+	private volatile int permits;
+    /**
+     * Constructor initialize the data members.  
+     */
+    public SimpleSemaphore (int permits,
+                            boolean fair)
+    { 
+        // TODO - you fill in here
+    	this.permits = permits;
+    	lock = new ReentrantLock(fair);
+    	condition = lock.newCondition();
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that can be
-     * interrupted.
+     * Acquire one permit from the semaphore in a manner that can
+     * be interrupted.
      */
     public void acquire() throws InterruptedException {
-        // TODO - you fill in here.
+        // TODO - you fill in here
+    	lock.lockInterruptibly();
+    	try{
+    		while(permits == 0){
+    			condition.await();
+    		}
+    		permits = permits - 1;
+    	}finally{
+    		lock.unlock();
+    	}
     }
 
     /**
-     * Acquire one permit from the semaphore in a manner that cannot be
-     * interrupted.
+     * Acquire one permit from the semaphore in a manner that
+     * cannot be interrupted.
      */
     public void acquireUninterruptibly() {
-        // TODO - you fill in here.
+        // TODO - you fill in here
+    	lock.lock();
+    	try{
+    		while(permits == 0){
+    			condition.awaitUninterruptibly();
+    		}
+    		permits = permits - 1;
+    	}finally{
+    		lock.unlock();
+    	}
     }
 
     /**
      * Return one permit to the semaphore.
      */
     void release() {
-        // TODO - you fill in here.
+        // TODO - you fill in here
+    	lock.lock();
+    	try{
+    		permits = permits + 1;
+    		condition.signalAll();
+    	}finally{
+    		lock.unlock();
+    	}
     }
-
+    
     /**
      * Return the number of permits available.
      */
-    public int availablePermits() {
-        // TODO - you fill in here to return the correct result
-    	return 0;
+    public int availablePermits(){
+    	// TODO - you fill in here
+    	int result = 0;
+    	lock.lock();
+    	try{
+    		result = permits;
+    	}finally{
+    		lock.unlock();
+    	}
+    	return result; // You will change this value. 
     }
 }
+
